831cbf7e160ba83bcf866a1d09b2e444fac15fdf
Altazi-Hussein <ali.hussein-@hotmail.fr>
2022-01-05 14:59:43 +0100

    installation vuestic et tests appels API

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,9 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;1,700&display=swap');
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
