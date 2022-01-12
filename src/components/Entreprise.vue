337cdbe65e84b0a99cbb86a3d122f4ca994ee284
Julien GODEST <julien.godest@etu.univ-lyon1.fr>
2022-01-12 14:27:11 +0100

    c

---
+++ src/components/Entreprise.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="bg-yellow-400">
+    <h1>
+      Entreprise
+    </h1>
+
+    <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>Opérations</th>
+                    <th>Entreprise</th>
+                    <th>Responsable</th>
+                    <th>Adresse</th>
+                    <th>Site</th>
+                    <th>Spécialité</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="entreprise in entreprises" :key="entreprise.id">
+                    <td></td>
+                    <td>{{entreprise.raisonSociale}}</td>
+                    <td>{{entreprise.nomResp}}</td>
+                    <td><p>{{entreprise.rueEntreprise}}</p><p>{{entreprise.cpEntreprise}} {{ entreprise.villeEntreprise}}</p></td>
+                    <td>{{entreprise.siteEntreprise}}</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: "Entreprise",
+
+  data() {
+    return {
+      entreprises: [],
+    };
+  },
+
+  created() {
+    this.getEntreprises();
+  },
+
+  methods: {
+    async getEntreprises() {
+      try {
+        const response = await axios.get(
+          "http://localhost:8080/stage/entreprise/all"
+        );
+
+        this.entreprises = response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
 import Stagiaire from '../components/Stagiaire.vue'
+import Entreprise from '../components/Entreprise.vue'
 import Login from '../components/Login.vue'
 
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: HelloWorld
+  },
+  {
+    path: '/entreprise',
+    name: 'Entreprise',
+    component: Entreprise
   },
   {
     path: '/stagiaire',
