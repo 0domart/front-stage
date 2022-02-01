46f63dc118cc67957d69b56dffb5f0fece867733
Julien GODEST <julien.godest@etu.univ-lyon1.fr>
2022-02-01 18:25:16 +0100

    etudiant description

--- src/components/CreerEtudiant.vue
+++ src/components/CreerEtudiant.vue
@@ -1,22 +1,84 @@
 <template>
-  <div class="bg-yellow-400">
-    <h1>
-      Informations concernant l'étudiant
-    </h1>
+  <div
+    class="
+      p-10
+      h-full
+      flex flex-col
+      items-center
+      space-y-20
+    "
+  >
+    <h1 class="text-3xl font-bold">Informations concernant l'étudiant</h1>
+
+<form>
+  <div class="bg-gray-100 border-2 border-gray-900 rounded-xl flex space-x-10 p-10">
+      <div class="flex flex-col space-y-5">
+        <label class="p-3 flex-grow text-left" for="nomEtudiant">Nom</label>
+        <label class=" p-3 flex-grow text-left" for="prenomEtudiant">Prénom</label>
+        <label class=" p-3 flex-grow text-left" for="login">Nom d'utilisateur</label>
+        <label class=" p-3 flex-grow text-left" for="password">Mot de passe</label>
+        <label class=" p-3 flex-grow text-left" for="dateObtention">Date d'obtention du BTS (JJ-MM-AAAA)</label>
+        <label class=" p-3 flex-grow text-left" for="classe">Classe</label>
+      </div>
 
 
-    <p>Nom : {{etudiant.nomEtudiant}}</p>
-    <p>Prénom : {{etudiant.prenomEtudiant}}</p>
-    <p>Nom d'utilisateur (8 caractères)* : {{etudiant.login}}</p>
-    <p>Mot de passe (entre 8 et 30 caractères)* : {{etudiant.mdp}}</p>
-    <p>Date d'obtention du BTS (AAAA-MM-JJ) : {{etudiant.siteEntreprise}}</p>
-    <p>Classe : {{etudiant.numClasse}}</p>
+<div class="flex flex-col space-y-5">
+        <input
+          class=" p-3 flex-grow rounded border-2 border-gray-400 transition hover:border-gray-800"
+          type="text"
+          id="nomEtudiant"
+          v-model="this.etudiant.nomEtudiant"
+          placeholder="Entrez un nom"
+        />
 
+        
+        <input
+          class=" p-3 flex-grow rounded border-2 border-gray-400 transition hover:border-gray-800"
+          type="text"
+          id="prenomEtudiant"
+          v-model="this.etudiant.prenomEtudiant"
+          placeholder="Entrez un prénom"
+        />
+
+        <input
+          class=" p-3 flex-grow rounded border-2 border-gray-400 transition hover:border-gray-800"
+          type="text"
+          id="login"
+          v-model="this.etudiant.login"
+          placeholder="Entrez un login"
+        />
+
+        <input
+          class=" p-3 flex-grow rounded border-2 border-gray-400 transition hover:border-gray-800"
+          type="password"
+          id="password"
+          v-model="this.etudiant.mdp"
+          placeholder="Entrez un mot de passe"
+        />
+
+
+        <input
+          class=" p-3 flex-grow rounded border-2 border-gray-400 transition hover:border-gray-800"
+          type="date"
+          id="dateObtention"
+          v-model="this.etudiant.anneeObtention"
+        />
+
+        <input
+          class=" p-3 flex-grow rounded border-2 border-gray-400 transition hover:border-gray-800"
+          type="text"
+          id="classe"
+          v-model="this.etudiant.numClasse"
+          placeholder="Entrez un numéro de classe"
+        />
+</div>
+  </div>
+  <input class="py-3 px-16 rounded-full m-5 font-bold cursor-pointer bg-gray-100 border-2 border-gray-900 hover:border-4 hover:text-white hover:bg-gray-900 duration-200 ease-in-out focus:ring-2" type="submit" value="Ajouter">
+    </form>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
 
 export default {
   name: "EtudiantDescription",
@@ -28,22 +90,10 @@ export default {
   },
 
   created() {
-    this.getEtudiant();
   },
 
   methods: {
-    async getEtudiant() {
-      try {
-        const id = this.$route.params.id;
-        const response = await axios.get(
-          "http://localhost:8080/stage/etudiant/" + id
-        );
-        console.log(response.data);
-        this.etudiant = response.data;
-      } catch (error) {
-        console.log(error);
-      }
-    },
+
   },
 };
 </script>
--- src/components/EtudiantDescription.vue
+++ src/components/EtudiantDescription.vue
@@ -16,7 +16,6 @@
         <label class="p-3 flex-grow text-left" for="nomEtudiant">Nom</label>
         <label class=" p-3 flex-grow text-left" for="prenomEtudiant">Prénom</label>
         <label class=" p-3 flex-grow text-left" for="login">Nom d'utilisateur</label>
-        <label class=" p-3 flex-grow text-left" for="password">Mot de passe</label>
         <label class=" p-3 flex-grow text-left" for="dateObtention">Date d'obtention du BTS (JJ-MM-AAAA)</label>
         <label class=" p-3 flex-grow text-left" for="classe">Classe</label>
       </div>
@@ -24,18 +23,20 @@
 
 <div class="flex flex-col space-y-5">
         <input
-          class=" p-3 flex-grow rounded border-2 border-gray-400 transition hover:border-gray-800"
+          class=" p-3 flex-grow rounded border-2 border-gray-400"
           type="text"
           id="nomEtudiant"
+          disabled
           v-model="this.etudiant.nomEtudiant"
           placeholder="Entrez un nom"
         />
 
         
         <input
-          class=" p-3 flex-grow rounded border-2 border-gray-400 transition hover:border-gray-800"
+          class=" p-3 flex-grow rounded border-2 border-gray-400"
           type="text"
           id="prenomEtudiant"
+          disabled
           v-model="this.etudiant.prenomEtudiant"
           placeholder="Entrez un prénom"
         />
@@ -44,36 +45,20 @@
           class=" p-3 flex-grow rounded border-2 border-gray-400 transition hover:border-gray-800"
           type="text"
           id="login"
+          disabled
           v-model="this.etudiant.login"
           placeholder="Entrez un login"
         />
 
-        <input
-          class=" p-3 flex-grow rounded border-2 border-gray-400 transition hover:border-gray-800"
-          type="password"
-          id="password"
-          v-model="this.etudiant.mdp"
-          placeholder="Entrez un mot de passe"
-        />
+        <p
+          class=" p-3 flex-grow rounded border-2 border-gray-400"
+        >{{this.etudiant.anneeObtention}}</p>
 
-
-        <input
-          class=" p-3 flex-grow rounded border-2 border-gray-400 transition hover:border-gray-800"
-          type="date"
-          id="dateObtention"
-          v-model="this.etudiant.anneeObtention"
-        />
-
-        <input
-          class=" p-3 flex-grow rounded border-2 border-gray-400 transition hover:border-gray-800"
-          type="text"
-          id="classe"
-          v-model="this.etudiant.numClasse"
-          placeholder="Entrez un numéro de classe"
-        />
+        <p
+          class=" p-3 flex-grow rounded border-2 border-gray-400"
+        >{{this.etudiant.numClasse}}</p>
 </div>
   </div>
-  <input class="py-3 px-16 rounded-full m-5 font-bold cursor-pointer bg-gray-100 border-2 border-gray-900 hover:border-4 hover:text-white hover:bg-gray-900 duration-200 ease-in-out focus:ring-2" type="submit" value="Ajouter">
     </form>
   </div>
 </template>
