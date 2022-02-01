4e4ed00fc453a450fbbdb9baf24d1fe53307a3e1
SOLChamies <94569526+SOLChamies@users.noreply.github.com>
2022-02-01 17:36:25 +0100

    ajouts graphiques

--- src/components/Entreprise.vue
+++ src/components/Entreprise.vue
@@ -111,12 +111,12 @@
 
               <a
                 v-if="isProfesseur"
-                class="p-2 border-2"
+                class="p-2"
                 href="#"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5"
+                  class="h-6 w-6"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                 >
@@ -128,13 +128,13 @@
 
               <a
                 v-if="isProfesseur"
-                class="p-2 border-red-500 border-2"
+                class="p-2"
                 href="#"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   class="h-6 w-6"
-                  fill="none"
+                  fill="black"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
