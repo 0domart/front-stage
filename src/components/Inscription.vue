2372eb66954977cbf6c0136af9cc2514a56e9c37
SOLChamies <94569526+SOLChamies@users.noreply.github.com>
2022-02-03 20:55:21 +0100

    message d'erreur inscription + style

--- src/components/Inscription.vue
+++ src/components/Inscription.vue
@@ -1,94 +1,100 @@
 <template>
-  <div class="p-10 flex flex-col justify-center items-center h-full">
-    <div
-      class="shadow-xl bg-gray-300 rounded w-96 p-10 flex flex-col space-y-6"
-    >
-      <h1 class="font-bold text-2xl ">Inscription</h1>
-      <div class="flex flex-col justify-start space-y-2">
-        <label class="self-start" for="login">Entreprise</label>
-        <select class="p-2.5 rounded" v-model="entrepriseSelected">
-          <option
-            v-for="entreprise in entreprises"
-            :value="entreprise"
-            :key="entreprise"
-          >
-            {{ entreprise.raisonSociale }}
-          </option>
-        </select>
-      </div>
+  <div class="p-10 flex flex-col justify-center items-center space-y-5 h-full">
+    <h3 v-show="error" @click="error=null" class="cursor-pointer border-2 border-red-500 bg-red-200 rounded-xl p-2">
+      L'inscription a echoué, merci de contacter le support
+    </h3>
+    <div class="shadow-xl bg-green-300 rounded-xl p-8 flex flex-col">
+      <h1 class="font-bold text-2xl">Inscription</h1>
+      <div class="grid grid-cols-2 gap-5">
+        <div class="flex flex-col justify-start space-y-2">
+          <label class="self-start" for="login">Entreprise</label>
+          <select class="p-2.5 rounded" v-model="entrepriseSelected">
+            <option
+              v-for="entreprise in entreprises"
+              :value="entreprise"
+              :key="entreprise"
+            >
+              {{ entreprise.raisonSociale }}
+            </option>
+          </select>
+        </div>
 
-      <div class="flex flex-col justify-start space-y-2">
-        <label class="self-start" for="login">Etudiant</label>
-        <select class="p-2.5 rounded" v-model="etudiantSelected">
-          <option
-            v-for="etudiant in etudiants"
-            :value="etudiant"
-            :key="etudiant"
-          >
-            {{ etudiant.nomEtudiant }} {{ etudiant.prenomEtudiant }}
-          </option>
-        </select>
-      </div>
+        <div class="flex flex-col justify-start space-y-2">
+          <label class="self-start" for="login">Etudiant</label>
+          <select class="p-2.5 rounded" v-model="etudiantSelected">
+            <option
+              v-for="etudiant in etudiants"
+              :value="etudiant"
+              :key="etudiant"
+            >
+              {{ etudiant.nomEtudiant }} {{ etudiant.prenomEtudiant }}
+            </option>
+          </select>
+        </div>
 
-      <div class="flex flex-col justify-start space-y-2">
-        <label class="self-start" for="login">Professeur</label>
-        <select class="p-2.5 rounded" v-model="professeurSelected">
-          <option
-            v-for="professeur in professeurs"
-            :value="professeur"
-            :key="professeur"
-          >
-            {{ professeur.nomProf }} {{ professeur.prenomProf }}
-          </option>
-        </select>
-      </div>
+        <div class="flex flex-col justify-start space-y-2">
+          <label class="self-start" for="login">Professeur</label>
+          <select class="p-3 rounded" v-model="professeurSelected">
+            <option
+              v-for="professeur in professeurs"
+              :value="professeur"
+              :key="professeur"
+            >
+              {{ professeur.nomProf }} {{ professeur.prenomProf }}
+            </option>
+          </select>
+        </div>
 
-      <div class="flex flex-col justify-start space-y-2">
-        <label class="self-start" for="login">Date de début de stage</label>
-        <input class="p-2.5 rounded" type="date" v-model="dtDebut" />
-      </div>
+        <div class="flex flex-col justify-start space-y-2">
+          <label class="self-start" for="login">Date de début de stage</label>
+          <input class="p-2.5 rounded" type="date" v-model="dtDebut" />
+        </div>
 
-      <div class="flex flex-col justify-start space-y-2">
-        <label class="self-start" for="login">Date de fin de stage</label>
-        <input class="p-2.5 rounded" type="date" v-model="dtFin" />
-      </div>
+        <div class="flex flex-col justify-start space-y-2">
+          <label class="self-start" for="login">Date de fin de stage</label>
+          <input class="p-2.5 rounded" type="date" v-model="dtFin" />
+        </div>
 
-      <div class="flex flex-col justify-start space-y-2">
-        <label class="self-start" for="login">Type de stage</label>
-        <select class="p-2.5 rounded" v-model="type">
-          <option value="aucun">Aucun</option>
-          <option value="stage">Stage</option>
-          <option value="alternance">Alternance</option>
-        </select>
-      </div>
+        <div class="flex flex-col justify-start space-y-2">
+          <label class="self-start" for="login">Type de stage</label>
+          <select class="p-2.5 rounded" v-model="type">
+            <option value="aucun">Aucun</option>
+            <option value="stage">Stage</option>
+            <option value="alternance">Alternance</option>
+          </select>
+        </div>
 
-      <div class="flex flex-col justify-start space-y-2">
-        <label class="self-start" for="login">Description du projet</label>
-        <input class="rounded p-2" v-model="description" type="text" />
-      </div>
+        <div class="flex flex-col justify-start space-y-2">
+          <label class="self-start" for="login">Description du projet</label>
+          <input class="rounded p-2.5" v-model="description" type="text" />
+        </div>
 
-      <div class="flex flex-col justify-start space-y-2">
-        <label class="self-start" for="login">Observation</label>
-        <input class="rounded p-2" v-model="observation" type="text" />
-      </div>
+        <div class="flex flex-col justify-start space-y-2">
+          <label class="self-start" for="login">Observation</label>
+          <input class="rounded p-2.5" v-model="observation" type="text" />
+        </div>
 
-      <button
-        class="
-          text-gray-800
-          border-2
-          p-2
-          border-gray-500
-          rounded
-          transition
-          hover:bg-gray-500 hover:text-white
-        "
-        @click="inscription"
-      >
-        Inscrire
-      </button>
-      <h3 v-show="error">
-        L'inscription a echoué, merci de contacter le support
-      </h3>
+        <button
+          class="
+            text-green-800
+            font-bold
+            uppercase
+            text-xl
+            border-2
+            mt-8
+            p-2.5
+            border-green-800
+            rounded
+            transition
+            ease-in-out
+            hover:bg-green-700 hover:text-white
+            col-span-2
+          "
+          @click="inscription"
+        >
+          Inscrire
+        </button>
+      </div>
     </div>
   </div>
 </template>
